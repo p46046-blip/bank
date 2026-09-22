ab4737f73900dc0d370a8c22e4bbd504136c17e5
p46046-blip <p46046@irma.ac.in>
2026-09-23 00:11:38 +0530

    Update churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+model = joblib.load('bank.sav')
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.linear_model import LogisticRegression
